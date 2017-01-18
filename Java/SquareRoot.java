a8a203cc94344acdd0fc9827744a07f3fe5e1ff8
Tianran Chen <tristan20177@gmail.com>
2017-01-17 19:03:06 -0800

    Add files via upload

--- Java/SquareRoot.java
+++ Java/SquareRoot.java
@@ -22,4 +22,27 @@ public class SquareRoot {
 		}
 		return right * right <= num ? right : left;
 	}
+
+	public int sqrtOpti(int num) {
+		if(num <= 1) {
+			return num;
+		}
+		int left = 1;
+		int right = 1;
+		while(right * right < num) {
+			right *= 2;
+		}
+		while(left < right - 1) {
+			int mid = left + (right - left) / 2;
+			if(mid * mid == num) {
+				return mid;
+			} else if (mid * mid < num) {
+				left = mid;
+			} else {
+				right = mid;
+			}
+		}
+
+		return right * right <= num ? right : left;
+	}
 }
