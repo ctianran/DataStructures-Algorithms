e1e93ede31299a3127a2ce390ed7f8dd47c0cbb3
Tianran Chen <tristan20177@gmail.com>
2017-01-18 12:51:04 -0800

    Add files via upload

--- Java/LongestSubstringNonRepeatingCharacter.java
+++ Java/LongestSubstringNonRepeatingCharacter.java
@@ -1,13 +1,15 @@
 package com.chentianran;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 /**
  * Created by Tianran on 1/18/2017.
  */
 public class LongestSubstringNonRepeatingCharacter {
-	public int longest(String input) {
+	public int longestI(String input) {
 		if(input == null || input.length() == 0) {
 			return 0;
 		}
@@ -25,4 +27,38 @@ public class LongestSubstringNonRepeatingCharacter {
 		}
 		return result;
 	}
+
+	public String longestII(String input) {
+		if(input == null || input.length() <= 1) {
+			return input;
+		}
+		Map<Character, Integer> map = new HashMap<>();
+		int slow = 0;
+		int result = 0;
+		int start = 0;
+		int end = 0;
+		for(int fast = 0; fast < input.length(); fast++) {
+			char ch = input.charAt(fast);
+			Integer count = map.get(ch);
+			if(count != null ) {
+					map.put(ch, count + 1);
+					if(count == 1) {
+						while(map.get(ch) > 1) {
+							Integer countSlow = map.get(input.charAt(slow));
+							map.put(input.charAt(slow++), countSlow - 1);
+						}
+					}
+			} else {
+				map.put(ch, 1);
+			}
+
+			if(fast - slow > result) {
+				result = fast - slow;
+				start = slow;
+				end = fast;
+			}
+		}
+		return input.substring(start, end + 1);
+	}
+
 }
